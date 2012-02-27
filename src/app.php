1fcc1315b5abe6e6b3a03e4be7f4e30999bc392a
Al Ganiev <helios.ag@gmail.com>
2012-02-27 23:29:28 +1100

    updated to latest silex version

--- src/app.php
+++ src/app.php
@@ -4,74 +4,67 @@ require_once __DIR__.'/../vendor/Silex/silex.phar';
 
 /**  Bootstraping */
 
-$app = new Silex\Application;
+use Silex\Provider\FormServiceProvider;
 
-use Silex\Extension\SymfonyBridgesExtension;
-use Silex\Extension\TranslationExtension;
-use Silex\Extension\FormExtension;
-use Silex\Extension\TwigExtension;
+$app = new Silex\Application();
 
-$app->register(new TwigExtension(), array(
-    'twig.path'       => array(
-	  __DIR__.'/templates',
-	  __DIR__.'/../vendor/symfony/src/Symfony/Bridge/Twig/Resources/views/Form'
-	),
-    'twig.class_path' => __DIR__.'/../vendor/Twig/lib',
+$app->register(new Silex\Provider\SymfonyBridgesServiceProvider(), array(
+    'symfony_bridges.class_path'  => __DIR__.'/../vendor/symfony/src',
 ));
 
-$app->register(new SymfonyBridgesExtension(), array(
-   'symfony_bridges.class_path' => __DIR__ . '/../vendor/symfony/src'
-));
-
-$app->register(new FormExtension(), array(
+$app->register(new Silex\Provider\FormServiceProvider(), array(
     'form.class_path' => __DIR__ . '/../vendor/symfony/src'
 ));
 
-$app->register(new TranslationExtension(), array(
-    'translation.class_path' => __DIR__ . '/../vendor/symfony/src',
-    'translator.messages' => array()
+$app->register(new Silex\Provider\TranslationServiceProvider(), array(
+    'locale_fallback'           => 'en',
+    'translation.class_path'    => __DIR__.'/../vendor/symfony/src',
 ));
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path'       => __DIR__.'/views',
+    'twig.class_path' => __DIR__.'/../vendor/twig/lib',
+));
+
+
+$app['translator.messages'] = array();
+
+$app['debug'] = true;
 
 /** App definition */
 
-$app->error(function(Exception $e) use ($app){
-    if (!in_array($app['request']->server->get('REMOTE_ADDR'), array('127.0.0.1', '::1'))) {
-        return $app->redirect('/');
-    }
-});
+$app->match('/', function(Silex\Application $app){
 
-$app->match('/', function() use ($app) {
-    
-    $form = $app['form.factory']->createBuilder('form')	
+    $form = $app['form.factory']->createBuilder('form')
         ->add('FileUpload', 'file')
-    ->getForm();
-    
-    $request = $app['request'];
-     
-    if ($request->getMethod() == 'POST')
-    {
-        $form->bindRequest($request);
-        if ($form->isValid())
-        {                         
-                $files = $request->files->get($form->getName());
-                /* Make sure the Upload Directory is properly configured and writeable */
-                $path = __DIR__.'/../web/upload/';                
-                $filename = $files['FileUpload']->getClientOriginalName();
-                $files['FileUpload']->move($path,$filename);
-                               
-        }
-        return $app['twig']->render('index.html.twig', array(
-                                   'message' => 'File Uploaded',
-                                   'form' => $form->createView()        
-                                    ));       
+        ->getForm();
 
-    }
+    $request = $app['request'];
+
+        if ($request->getMethod() == 'POST')
+        {
+            $form->bindRequest($request);
+            if ($form->isValid())
+            {
+                    $files = $request->files->get($form->getName());
+                    /* Make sure that Upload Directory is properly configured and writable */
+                    $path = __DIR__.'/../web/upload/';
+                    $filename = $files['FileUpload']->getClientOriginalName();
+                    $files['FileUpload']->move($path,$filename);
+
+            }
+            return $app['twig']->render('index.html.twig', array(
+                                       'message' => 'File Uploaded',
+                                       'form' => $form->createView()
+                                        ));
+
+        }
     return $app['twig']->render('index.html.twig', array(
-	'message' => 'Upload a file',
-        'form' => $form->createView()        
+            'message' => 'Upload a file',
+            'form' => $form->createView()
         )
-            );    
-}, "GET|POST");
+    );
+}, 'GET|POST');
 
 
 return $app;
